50aeaa928052d6ccb437284f88ac4f35d0841507
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-27 19:18:01 -0300

    Update regularExpressions.php

--- Study/Advanced/RegularExpressions/regularExpressions.php
+++ Study/Advanced/RegularExpressions/regularExpressions.php
@@ -1,2 +1,14 @@
 <?php
 
+$test = 'Gabriel';
+
+// if the test variable has the expression "iel" echo true
+if (preg_match('/iel/', $test)) {
+    echo 'true';
+}
+
+echo "\n====\n";
+
+// just use the question mark when you know what comes next
+preg_match('/(.*?)bri(.*)/', $test, $request);
+echo $request[0];
