6c4d14ca7dbbf89db5b02a78373b525febe9e0eb
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-27 21:00:32 -0300

    prag_match

--- Study/Advanced/RegularExpressions/regularExpressions.php
+++ Study/Advanced/RegularExpressions/regularExpressions.php
@@ -7,8 +7,40 @@ if (preg_match('/iel/', $test)) {
     echo 'true';
 }
 
-echo "\n====\n";
+echo "<br>================<br>";
 
 // just use the question mark when you know what comes next
 preg_match('/(.*?)bri(.*)/', $test, $request);
-echo $request[0];
+
+echo "<pre>";
+var_dump($request);
+echo "</pre>";
+
+echo "<br>========CPF Validation=======<br>";
+
+if (isset($_POST['submit'])) {
+    $cpf = $_POST['cpf'];
+
+    if (isCPFValid($cpf)) {
+        echo 'true';
+    } else {
+        echo 'false';
+    }
+}
+
+function isCPFValid(string $cpf): bool {
+    // CPF Format: 000.000.000-00;
+
+    $expression = '/[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/'; // [0-9a-z*/._-=!,-+]
+
+    return preg_match($expression, $cpf);
+}
+
+?>
+
+<form method="post">
+
+    <input type="text" name="cpf">
+    <input type="submit" name="submit">
+
+</form>
