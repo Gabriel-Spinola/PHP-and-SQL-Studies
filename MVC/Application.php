dfbe3971239091ec1493072c8f6d36cf92ba1db9
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-02 14:54:56 -0300

    Update Application.php

--- MVC/Application.php
+++ MVC/Application.php
@@ -4,9 +4,25 @@ class Application {
     /**
      * @return void
      * 
-     * Execute the program. 
+     * Execute the program.
+     * - Get and format url
+     * - Check if we have a controller to the url and run it.
     */
     public function execute(): void {
-        echo 'execute';
+        // in the case of "page1/section1", we only get the "page1"
+        $url = isset($_GET['url']) ? explode('/', $_GET['url'])[0] : 'home';
+        $url = ucfirst($url);
+
+        $url .= "Controller";
+
+        if (file_exists('Controllers/' . $url . '.php')) {
+            echo 'loading class: ' . $url;
+
+           $controller = new $url();
+
+           $controller -> execute();
+        } else {
+            die ("ERROR::APPLICATION | Controller doesn't exists");
+        }
     }
 }
