90f47934740bbeebdf1c24bd5df80a7612833cb2
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-02 16:09:05 -0300

    Update MainView.php

--- MVC/Views/MainView.php
+++ MVC/Views/MainView.php
@@ -5,16 +5,22 @@ namespace Views;
 class MainView {
     public function __construct(
         private $fileName,
-        private $header = 'header.php',
-        private $footer = 'footer.php',
+        private $header = 'header',
+        private $footer = 'footer',
     ) { }
 
     /**
      * @return void
      * 
      * Render the target ($this->fileName) Page
+     * 
+     * - Include the header (custom or default)
+     * - Include the target Page
+     * - Include the footer (custom or default)
     */
     public function render(): void {
-        include 'pages/' . $this -> fileName . '.php';
+        include 'Pages/Templates/' . $this -> header . '.php';
+        include 'Pages/' . $this -> fileName . '.php';
+        include 'Pages/Templates/' . $this -> footer . '.php';
     }
 }
