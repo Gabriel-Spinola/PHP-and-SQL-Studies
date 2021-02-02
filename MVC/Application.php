cf9d41b5d3889b2aa8ee5932f2c9fca8d188d196
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-02 14:45:37 -0300

    Setup Project

--- MVC/Application.php
+++ MVC/Application.php
@@ -1,5 +1,12 @@
 <?php
 
 class Application {
-    
+    /**
+     * @return void
+     * 
+     * Execute the program. 
+    */
+    public function execute(): void {
+        echo 'execute';
+    }
 }
--- MVC/index.php
+++ MVC/index.php
@@ -14,3 +14,23 @@
  *  Contact View => Render the page.
  *  Contact Model => Store the required function
 */
+
+// ===========================
+// Autoload
+
+/**
+ * @return void
+ * @param string class
+ * 
+ * include classes based on their name
+*/
+$autoload = function(string $class): void {
+    include $class . '.php';
+};
+
+spl_autoload_register($autoload);
+
+// ===========================
+// Load Application
+$app = new Application();
+$app -> execute();
