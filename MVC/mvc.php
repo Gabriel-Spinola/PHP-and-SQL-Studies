d3dae866fadf73a6e29f394cb58c3a29a6bbaef2
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-02 14:34:29 -0300

    Update mvc.php

--- MVC/mvc.php
+++ MVC/mvc.php
@@ -1,10 +1,19 @@
 <?php
 
+## index file ##
+
 /**
  * MVC
  * 
  * M: MODEL
  * V: VIEW
  * C: CONTROLLER
+ * 
+ * ================
+ * 
+ * /Contact Page
+ *  Contact Controller => General Controller Class. (Execute the model and the view).
+ *  Contact View => Render the page.
+ *  Contact Model => Store the required function
 */
 
