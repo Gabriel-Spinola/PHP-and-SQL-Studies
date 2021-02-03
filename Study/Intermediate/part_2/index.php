2abd98441818098a0095ac9cf46fc53c2d58982b
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-03 12:02:41 -0300

    Update index.php

--- Study/Intermediate/part_2/index.php
+++ Study/Intermediate/part_2/index.php
@@ -1,39 +1,8 @@
 <?php
 
-$helloWorld = ['H' , '-' , 'E' , '-' , 'L' , '-' , 'L' , '-' , 'O'];
-$string = '';
-
-for($i = 0; $i < count($helloWorld); $i++) {
-    if($helloWorld[$i] == '-') {
-        continue;
-    }
-
-    $string .= $helloWorld[$i];
-}
-
-echo "<br>";
-
-echo $string . "<br>" . "<hr>" . "<br>";
-
-$arr = ['key1' => 'Guilherme' , 'João' , 'Felipe' , 'Mario'];
-
-$arr1 = array('G' , 'J' , 'F' , 'M');
-
-$total = count($arr1);
-
-for($i = 0; $i < $total; $i++) {
-    echo $arr1[$i] . "<hr>" . "<br>";
-}
-
-echo 'oi'. "<br>" . "<br>";
-
-
-$arr2 = array(
-    ['Guilherme' , 'Jão'],
-    [23 , 45]
-);
-
-echo $arr2[0][0] . "<br>" . "<br>";
+/**
+ * 
+*/
 
 
 sleep(3);
