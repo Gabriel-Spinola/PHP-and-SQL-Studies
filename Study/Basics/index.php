c7be76d56e6ba90eba388f8cc55936eeab1e3698
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-03 11:44:54 -0300

    Update index.php

--- Study/Basics/index.php
+++ Study/Basics/index.php
@@ -1,28 +1,52 @@
 <?php
 
-#region Arrays
+#region Variables & Consts
 
-    $N = ['Gabriel' , 'Guilherme' , 'Filipe'];
-    echo $N[0].'<br>';
-    echo $N[1].'<br>';
-    echo $N[2].'<br>' . '<br>';
+/**
+ * Variables are used to store information,
+ * to be referenced and manipulated later.
+ * 
+ * We have 8 types of variables Such as:
+ * 
+ * byte: integers (8 bit, minimum value of -128, and maximum value of 127),
+ * short: integers (minimum value of -32,768 and a maximum value of 32,767)
+ * int: integers (32 bit, minimum value of -2^31, and a maximum value of 2^31 - 1),
+ * long: really long integers (64 bit, minimum value of -2^63 and a maximum value of 2^63 - 1)
+ * float: decimal numbers (32 bit),
+ * double: decimal numbers (64 bit),
+ * boolean: True or False (1 bit, 0 | 1),
+ * char: Single Unicode character (16 bit, minimum value of '\u0000' (or 0) and a maximum value of '\uffff' (or 65,535 inclusive))
+ * 
+ * And Non-primitive data types:
+ * 
+ * String: words and text (8 bit or 16 bit, based on text width)
+ * Arrays: a collection of elements (values or variables)
+ * 
+ * Also we have:
+ * Null: Nothing
+ * Object: stores not only data but also information on how to process that data
+ * 
+ * But php only uses:
+ * Strings, Int, float, Boolean, Array, Null and Object.
+*/
 
-    $information['n'] = 'G';
-    $information['i'] = 2;
-    $information['c'] = 'F';
-    echo $information['n'].'<br>';
-    echo $information['i'].'<br>';
-    echo $information['c'].'<br>' . '<br>';
+    $name = 'Gui'; // string variable 
+    echo 'My name is ' . $name . '<br>' . '<br>'; // printing variable
 
-#endregion
+    const NAME = 'Gabriel'; // constant
+    echo NAME.'<br>'; // printing constant
 
-#region Vars & Consts
+    $int = 1;
+    $string = "Hallo!";
+    $float = 12.5;
 
-    const NAME = 'Gabriel'; // Const
-    echo NAME.'<br>'; // Const dec
-
-    $name = 'Gui'; // var 
-    echo 'My name is ' . $name . '<br>' . '<br>'; // var dec
+    $boolean = true;
+    $boolean2 = false;
+    
+    $null = null;
+    
+    $array = [];
+    $array2 = array();
 
 #endregion
 
@@ -41,7 +65,7 @@
 
     $number = 5 + 5;
     $number1 = 10 - 4;
-    
+
     $result = $number * $number1;
     $result1 = $result / 5;
 
@@ -51,6 +75,24 @@
     $rest = 1 % 2;
 
     echo $expression.'<br>'.
-    $rest.'<br>' . '<br>';
+    $rest . '<br>' . '<br>';
+
+#endregion
+
+#region Arrays
+
+    $N = ['Gabriel' , 'Guilherme' , 'Filipe'];
+
+    echo $N[0].'<br>';
+    echo $N[1].'<br>';
+    echo $N[2].'<br>' . '<br>';
+
+    $information['n'] = 'G';
+    $information['i'] = 2;
+    $information['c'] = 'F';
+
+    echo $information['n'].'<br>';
+    echo $information['i'].'<br>';
+    echo $information['c'].'<br>' . '<br>';
 
 #endregion
