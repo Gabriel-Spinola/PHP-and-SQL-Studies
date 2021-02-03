c201f88eced0d212987179fb9e22bd50283df923
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-03 13:01:30 -0300

    Update index2.php

--- Fundamentals/Intermediate/part_5/index2.php
+++ Fundamentals/Intermediate/part_5/index2.php
@@ -1,15 +1,13 @@
 <?php
 
-    session_start();
+session_start();
 
-    if(isset($_SESSION['name'])) {
-        echo $_SESSION['name'];
-    }
+if(isset($_SESSION['name'])) {
+    echo $_SESSION['name'];
+}
 
-    // To Destroy a Variable Session:
-    // unset($_SESSION['nome']);
+// To Destroy a Variable Session:
+// unset($_SESSION['nome']);
 
-    // To Destroy the Session:
-    // session_destroy()
-
-?>
+// To Destroy the Session:
+// session_destroy()
