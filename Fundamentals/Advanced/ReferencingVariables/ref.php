e2d7c99b31ef23a7fd82f4db1b6990bba3d1ee96
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-11 14:04:53 -0300

    Explanation

--- Fundamentals/Advanced/ReferencingVariables/ref.php
+++ Fundamentals/Advanced/ReferencingVariables/ref.php
@@ -1,11 +1,12 @@
 <?php
 
+// You can pass a variable by reference to a function so the function can modify the variable.
+
 $name = 'John';
 
 function Test(string &$arg) {
     $arg = 'Jorge';
 }
 
-Test($name);
-
-echo $name;
+Test($name); // Pass John
+echo $name; // Returns Jorge
