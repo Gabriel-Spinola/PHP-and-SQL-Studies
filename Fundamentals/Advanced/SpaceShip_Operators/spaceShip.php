9ffe871011531392e7465ac20f2ff90576472518
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-11 13:59:00 -0300

    Create spaceShip.php

---
+++ Fundamentals/Advanced/SpaceShip_Operators/spaceShip.php
@@ -0,0 +1,18 @@
+<?php
+
+// The spaceship operator is used for comparing two expressions
+// Returning 1 if the left value is greater
+// Returning -1 if the right value is greater
+// Return 0 if the both have the same value
+
+$foo = 10 <=> 8; // returns 1
+$dib = 10 <=> 10; // return 0
+$bar = 8 <=> 10; // return -1
+
+echo $foo . "\n" . $dib . "\n" . $bar . "\n\n";
+
+// Also works with strings
+
+$mib = 'a' <=> 'z';
+
+echo $mib;
